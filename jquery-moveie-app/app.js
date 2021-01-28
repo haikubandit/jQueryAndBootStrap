d38069dd50239d3fcea2c74232bd151d719f5934
Daniel Knapp <knapparatus@gmail.com>
2021-01-27 18:20:40 -0600

    Title has to be > 2 characters, stars can be from 0 to 10

--- jquery-moveie-app/app.js
+++ jquery-moveie-app/app.js
@@ -24,13 +24,20 @@
 //
 // Add list item with movie and rating from form
 $('form').on('click', 'button', function(e) {
+	// prevent page from reloading on form submission
 	e.preventDefault();
+	// create variables for input values
 	const $title = $('#title').val();
 	const $rating = $('#rating').val();
-	$('ul').append(`<li><b>${$title}</b> is rated <b>${$rating}</b> stars  </li>`);
 
-	// Add delete button at end of each list item
-	$('ul li').append('<button>Delete</button>');
+	if ($title.length < 2) {
+		alert('Title not long enough!');
+	} else {
+		$('ul').append(`<li><b>${$title}</b> is rated <b>${$rating}</b> stars  </li>`);
+
+		// Add delete button at end of each list item
+		$('ul li').append('<button>Delete</button>');
+	}
 });
 
 // remove list item when delete button is clicked
